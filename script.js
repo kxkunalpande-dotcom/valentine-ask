5a82e41c93a01801d88c913853b2b75618aee3c0
Kunal P <kx.kunalpande@gmail.com>
2026-02-01 16:22:40 -0600

    textch

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "YIPPIEEE!! I KNEW YOU'D DAY YES!! ^w^";
 
     catImg.src = "cat_dance.gif";
 
